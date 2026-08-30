77407cdeecc746352f426349ae49706846212da5
Jprimero15 <jprimero15>
2026-08-30 17:58:07 +0800

    improve float display

--- php_currency_converter.py
+++ php_currency_converter.py
@@ -5,12 +5,12 @@ amount = float(input("Amount:"))
 
 if choice == "usd":
     new_amount = amount * 62.27
-    print(f"{new_amount} php")
+    print(f"{new_amount:.2f} php")
 elif choice == "aud":
     new_amount = amount * 44.62
-    print(f"{new_amount} php")
+    print(f"{new_amount:.2f} php")
 elif choice == "eur":
     new_amount = amount * 72.44
-    print(f"{new_amount} php")
+    print(f"{new_amount:.2f} php")
 else:
     print("Invalid currency! Try again")
