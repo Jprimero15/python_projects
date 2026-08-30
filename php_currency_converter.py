27b41b33642c80a3a66f9aa398745b611cbacae1
Jprimero15 <jprimero15>
2026-08-30 17:44:19 +0800

    php_currency_converter.py: Fix indentation

--- php_currency_converter.py
+++ php_currency_converter.py
@@ -4,13 +4,13 @@ choice = str(input("Type your currency:")).lower()
 amount = float(input("Amount:"))
 
 if choice == "usd":
- new_amount = amount * 62.27
- print(f"{new_amount} php")
+    new_amount = amount * 62.27
+    print(f"{new_amount} php")
 elif choice == "aud":
- new_amount = amount * 44.62
- print(f"{new_amount} php")
+    new_amount = amount * 44.62
+    print(f"{new_amount} php")
 elif choice == "eur":
- new_amount = amount * 72.44
- print(f"{new_amount} php")
+    new_amount = amount * 72.44
+    print(f"{new_amount} php")
 else:
- print("Invalid currency! Try again")
+    print("Invalid currency! Try again")
