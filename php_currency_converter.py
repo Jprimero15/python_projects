7ccc4b9dfad07febf37b474cd377fb409d13ceed
Jprimero15 <jprimero15>
2026-08-31 19:22:19 +0800

    php_currency_converter.py: changes

--- php_currency_converter.py
+++ php_currency_converter.py
@@ -1,16 +1,19 @@
 print("Simple Offline PHP currency converter!\n(Currenly supported: USD, AUD, EUR)")
 
+def compute():
+    print(f"{new_amount:,.2f} php")
+
 choice = str(input("Type your currency:")).lower()
 amount = float(input("Amount:"))
 
 if choice == "usd":
     new_amount = amount * 62.27
-    print(f"{new_amount:.2f} php")
+    compute()
 elif choice == "aud":
     new_amount = amount * 44.62
-    print(f"{new_amount:.2f} php")
+    compute()
 elif choice == "eur":
     new_amount = amount * 72.44
-    print(f"{new_amount:.2f} php")
+    compute()
 else:
     print("Invalid currency! Try again")
