05a71bc32455b06b78eb5037a0eb06c1eeb39c6e
Jprimero15 <jprimero15>
2026-09-01 06:10:37 +0800

    guessing_game.py: optimize and add attempts
    lets add 10 attempts for now to make it exiciting
    
    TODO:
    - Remove global
    - Betting system(maybe)

--- guessing_game.py
+++ guessing_game.py
@@ -1,17 +1,29 @@
 import random
 
 number = random.randint(1, 100)
+attempt = 10
+
+def check_attempts():
+     global attempt
+     attempt -= 1
+     print(f"{attempt} attempt/s remaining")
 
 print("Number guessing game")
+print(f"You have {attempt} attempts")
+
 guess = int(input("Guess the number: "))
 
-while guess != number:
+while guess != number and attempt > 1:
  if guess > number:
-     print("Too high!")
+     print("Too high!", end=" ")
+     check_attempts()
      guess = int(input("Guess the number: "))
  else:
-     print("Too low!")
+     print("Too Low!", end=" ")
+     check_attempts()
      guess = int(input("Guess the number: "))
 
-print("Correct, You win!")
-
+if guess == number:
+	print("Correct, You win!")
+else:
+	print("Out of attempts, You lose")
